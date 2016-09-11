b5dc0641d5c7613fe637b6de148b27aa8cec51c0
Rene Kaufmann <kaufmann.r@gmail.com>
2016-09-11 19:38:20 +0200

    fixed duplicated data

--- logrus_influxdb.go
+++ logrus_influxdb.go
@@ -21,14 +21,14 @@ var (
 
 // InfluxDBHook delivers logs to an InfluxDB cluster.
 type InfluxDBHook struct {
-	sync.Mutex          // TODO: we should clean up all of these locks
-	client              influxdb.Client
+	sync.Mutex                       // TODO: we should clean up all of these locks
+	client                           influxdb.Client
 	precision, database, measurement string
-	tagList             []string
-	batchP              influxdb.BatchPoints
-	lastBatchUpdate     time.Time
-	batchInterval       time.Duration
-	batchCount          int
+	tagList                          []string
+	batchP                           influxdb.BatchPoints
+	lastBatchUpdate                  time.Time
+	batchInterval                    time.Duration
+	batchCount                       int
 }
 
 // NewInfluxDB returns a new InfluxDBHook.
@@ -59,7 +59,7 @@ func NewInfluxDB(config *Config, clients ...influxdb.Client) (hook *InfluxDBHook
 	hook = &InfluxDBHook{
 		client:        client,
 		database:      config.Database,
-		measurement:	 config.Measurement,
+		measurement:   config.Measurement,
 		tagList:       config.Tags,
 		batchInterval: config.BatchInterval,
 		batchCount:    config.BatchCount,
@@ -96,6 +96,7 @@ func (hook *InfluxDBHook) Fire(entry *logrus.Entry) (err error) {
 	for _, tag := range hook.tagList {
 		if tagValue, ok := getTag(entry.Data, tag); ok {
 			tags[tag] = tagValue
+			delete(map[string]interface{}(entry.Data), tag)
 		}
 	}
 
